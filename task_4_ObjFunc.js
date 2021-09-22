9bd7f503086eac9518d00a1613bb08f2e3bea67e
iok88 <6561657@gmail.com>
2021-09-22 23:43:24 +0300

    Do work task4

--- task_4_ObjFunc.js
+++ task_4_ObjFunc.js
@@ -1,16 +1,25 @@
-console.log("Hello, task 4");
-var y=0;
-let salaries = {
-
-  John: 100,
-  Ann: 160,
-  Pete: 130
-}
-for (x in salaries)
-{
-	
-	y+=salaries[x];
-}
-console.log(y);
-
-
+ console.log("4------------------------------------------------");
+ let car = {};
+ car.color = "black";
+ car.color = "green";
+ car.power = function showPower() {
+								   console.log("engien power 101 hp");
+								  }
+ car.power();
+ console.log("------------------------------------------------"); 
+ var a=15 // pears
+ var b=13 //apples
+ function calcFruit(x,y){
+	let z=x+y;
+	return(z);
+	}
+ console.log(calcFruit(a,b) + " total fruits ");
+ console.log("------------------------------------------------"); 
+  const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+readline.question("What's your name?", (name) => {
+console.log("Hi " + name);
+readline.close()
+})
