f5cbbb8d5822205ed160bb253ec2f94c04939c02
Локальный ГНС <D:\BAT(notDELET)>
2021-09-24 14:17:09 +0300

    task 4 1.4

--- task_4_ObjFunc.js
+++ task_4_ObjFunc.js
@@ -1,25 +1,54 @@
- console.log("4------------------------------------------------");
- let car = {};
- car.color = "black";
- car.color = "green";
- car.power = function showPower() {
-								   console.log("engien power 101 hp");
-								  }
- car.power();
- console.log("------------------------------------------------"); 
- var a=15 // pears
- var b=13 //apples
- function calcFruit(x,y){
-	let z=x+y;
-	return(z);
-	}
- console.log(calcFruit(a,b) + " total fruits ");
- console.log("------------------------------------------------"); 
-  const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-readline.question("What's your name?", (name) => {
-console.log("Hi " + name);
-readline.close();
-})
+    console.log("4------------------------------------------------");
+    let car = {};
+    car.color = "black";
+    car.color = "green";
+    car.power = function showPower() {
+		   						      console.log("engien power 101 hp");
+	   							     }
+    car.power();
+ 
+    console.log("------------------------------------------------"); 
+ 
+    var a=15 // pears
+    var b=13 //apples
+    function calcFruit(x,y){
+	   let z=x+y;
+	   return(z);
+	   }
+    console.log(calcFruit(a,b) + " total fruits ");
+ 
+    console.log("------------------------------------------------"); 
+ 
+    let myname = "Ivan"; // 
+    const readline = require('readline').createInterface({
+      input: process.stdin, 
+      output: process.stdout
+    }); 
+ 
+    const start = async () =>{
+ 	   console.log("What's your name?"  )
+        for await (const line of readline) {
+            if (line == myname) {
+	 	   console.log("Hello " + line);
+		    }
+	 	   else console.log("No such name ");
+		   readline.close();
+		} 
+    }
+    start()
+ 
+    console.log("------------------------------------------------"); 
+  
+  const stop = async () =>{
+ 	   console.log("HAHAHAHA?"  )
+        for await (const line of readline) {
+            if (line == myname) {
+	 	   console.log("Hello HAHAHA " + line);
+		   //readline.close();
+	   	    }
+	 	   else console.log("No such name HAHAHA ");
+		
+        } 
+    }
+    stop()
+
