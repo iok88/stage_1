c2c793348cc86b26ef56779a9a7e63e6079b0965
iok88 <6561657@gmail.com>
2021-09-27 23:44:19 +0300

    Go work 8 00

--- task_4_ObjFunc.js
+++ task_4_ObjFunc.js
@@ -3,8 +3,8 @@
     car.color = "black";
     car.color = "green";
     car.power = function showPower() {
-		   						      console.log("engien power 101 hp");
-	   							     }
+		console.log("engien power 101 hp");
+	}
     car.power();
  
     console.log("------------------------------------------------"); 
@@ -27,8 +27,38 @@
 		else console.log("No such name");	
 	}
 	checkName("Ivan");
+	
     console.log("------------------------------------------------"); 
     
+	function showType(name) {
+	c=typeof name;
+	console.log (c);
+	}
+	showType(123);
+	showType("Hi");
+	showType(true);
+	showType({});
+	
+	console.log("------------------------------------------------"); 
+	
+	function calcNumber(x){
+		
+		y=Math. trunc((Math.abs(x))**(1/2)); // take the integral part of the absolute 
+											//	value of the square root of the number
+		for (let i=2; i<=y; i++){
+			if (x%i==0 && x>3) {
+				console.log(x + "  HEPROSTOE");
+			}
+			else console.log(x + "  PROSTOE");
+		}
+	}
+	calcNumber(1);
+	calcNumber(2);
+	calcNumber(3);
+	calcNumber(4);
+	calcNumber(5);
+	calcNumber(5487);
+	
   
 
 
